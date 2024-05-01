fdda458e92908e5059d1bc4ae41fa8a5f61815d8
akktii <aktanmoldokeev123@gmail.com>
2024-05-01 22:59:23 +0600

    feat: implement api for the creation of the establishment profile

---
+++ src/shared/api/api/establishment.ts
@@ -0,0 +1,21 @@
+import { drinkjoyApi } from '../drinkjoyApi';
+
+export const createEstablishment = async (
+  url: string,
+  {
+    arg,
+  }: {
+    arg: {
+      owner: string;
+      name: string;
+      email: string;
+      latitude: string;
+      longitude: string;
+      phone_number: string;
+      happy_hour_start?: string;
+      happy_hour_end?: string;
+    };
+  },
+) => {
+  await drinkjoyApi.post(url, { ...arg });
+};
