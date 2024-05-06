e441b00dcead3297ec6ab84087ce059881548877
Ilias <ilias.kadyrkulov@gmail.com>
2024-05-06 16:37:02 +0600

    feat: admin auth; refactor: file structure to reuse login form

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from '@/features/login';
-
-export default function Page() {
-  return (
-    <div className="login flex min-h-dvh items-center justify-center">
-      <LoginForm />
-    </div>
-  );
-}
