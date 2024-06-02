f909286076069353aebd1ef32995be7b6e7e0aae
Ilias <ilias.kadyrkulov@gmail.com>
2024-06-02 14:48:32 +0600

    fix: cache revalidation on partner menu page

--- src/widgets/beverage-list/hooks/useMenu.ts
+++ src/widgets/beverage-list/hooks/useMenu.ts
@@ -2,18 +2,19 @@
 
 import useSWR from 'swr';
 
-import { fetcher, useLocalStorage } from '@/shared';
+import { fetcher } from '@/shared';
 import { TMenusResponse } from '@/widgets/beverage-list';
 
-export const useMenu = () => {
-  const [menuId] = useLocalStorage('menu_id', null); //FIXME - is null even though on "Create Menu" localStorage gets updated correctly
-
-  return useSWR<TMenusResponse>(`/menus/${menuId}`, fetcher, {
+export const useMenu = (menuId: number | null) => {
+  return useSWR<TMenusResponse>(`/menus/${menuId}/`, fetcher, {
     onSuccess: () => {
       console.log('success');
     },
     onError: () => {
       console.log('error');
     },
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
   });
 };
