f3a44b45a94e2314f9da116164ec3d43ebc0faec
Ilias <ilias.kadyrkulov@gmail.com>
2024-06-20 15:02:40 +0600

    fix: prerender error, style property type conflict in Textarea

--- src/app/admin/subscription/active_plans/page.tsx
+++ src/app/admin/subscription/active_plans/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
+
 import { Container } from '@/shared';
 import { SubscriptionList } from '@/widgets/subscription-list';
 
 export default function Page() {
   return (
     <Container title="Subscription plans">
-      <SubscriptionList />
+      <Suspense fallback={<div className="fixed inset-2/4">Loading...</div>}>
+        <SubscriptionList />
+      </Suspense>
     </Container>
   );
 }
--- src/shared/ui/Textarea/Textarea.tsx
+++ src/shared/ui/Textarea/Textarea.tsx
@@ -11,14 +11,17 @@ export const Textarea = (props: TextareaProps) => {
   return (
     <div className="space-y-2">
       {title ? <Typography variant="caption">{title}:</Typography> : null}
-
-      <TextareaAutosize
-        {...rest}
-        className={twMerge(
-          'block h-20 w-full resize-none rounded-md border border-theme-grey-200 bg-transparent px-4 py-2.5 text-sm text-theme-black outline-none placeholder:text-sm placeholder:text-theme-grey-400 focus-within:border-theme-grey-400 focus:ring-0 disabled:bg-theme-grey-300 disabled:placeholder:text-theme-grey-400',
-          className,
-        )}
-      />
+      {
+        //@ts-ignore
+        //FIXME - style property type conflict
+        <TextareaAutosize
+          {...rest}
+          className={twMerge(
+            'block h-20 w-full resize-none rounded-md border border-theme-grey-200 bg-transparent px-4 py-2.5 text-sm text-theme-black outline-none placeholder:text-sm placeholder:text-theme-grey-400 focus-within:border-theme-grey-400 focus:ring-0 disabled:bg-theme-grey-300 disabled:placeholder:text-theme-grey-400',
+            className,
+          )}
+        />
+      }
     </div>
   );
 };
