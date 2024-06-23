62dd7649fa1013f9c915a7a94472b8dd1604f807
Ilias <ilias.kadyrkulov@gmail.com>
2024-06-23 18:51:50 +0600

    feat: tabs in partner profile, change password form

--- src/widgets/partner-profile/ui/PartnerProfile.tsx
+++ src/widgets/partner-profile/ui/PartnerProfile.tsx
@@ -1,20 +1,31 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import { RiInformationLine, RiLockPasswordLine } from '@remixicon/react';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useSWRConfig } from 'swr';
 
+import { SubmitButton } from '@/features';
 import {
   Button,
+  Error,
   ESTABLISHMENT_EDIT_PATH,
   ESTABLISHMENT_PATH,
+  Input,
   Modal,
+  ShowPassword,
+  TChangePassForm,
   Typography,
+  useChangePassword,
   useChosenEstablishmentContext,
   useDeleteEstablishment,
 } from '@/shared';
+
+type TTab = 'info' | 'security';
+type TPassword = 'old' | 'new' | 'confirm';
 
 export const PartnerProfile = () => {
   const { chosenEstablishment, isChosenEstablishmentLoading } =
@@ -22,6 +33,10 @@ export const PartnerProfile = () => {
   const { data, trigger, error, isMutating } = useDeleteEstablishment(
     chosenEstablishment?.id,
   );
+  const { changePassword, changePasswordError, isPasswordChanging } =
+    useChangePassword();
+  const [chosenTab, setChosenTab] = useState<TTab>('info');
+  const [showPassword, setShowPassword] = useState<TPassword | null>(null);
   const { mutate } = useSWRConfig();
   const pathname = usePathname();
   const { push } = useRouter();
@@ -29,16 +44,37 @@ export const PartnerProfile = () => {
   const params = new URLSearchParams(searchParams);
   const establishmentId = 'establishmentId';
 
-  const onEstablishmentDelete = () => {
+  const onEstablishmentDelete = useCallback(() => {
     trigger();
-  };
-  const onModalOpen = () => {
+  }, []);
+  const onModalOpen = useCallback(() => {
     params.set(establishmentId, String(chosenEstablishment?.id));
-    push(`${pathname}?${params.toString()}`);
-  };
-  const onModalClose = () => {
+    console.log(params.get(establishmentId));
+
+    push(`${pathname}?${params.toString()}`, { scroll: false });
+  }, [params]);
+  const onModalClose = useCallback(() => {
     params.delete(establishmentId, '');
-    push(pathname);
+    push(pathname, { scroll: false });
+  }, []);
+
+  const onTabChoose = (tab: TTab) => {
+    setChosenTab(tab);
+  };
+  const onFormSubmit = async (
+    { old_password, password, confirm_password }: TChangePassForm,
+    { resetForm }: FormikHelpers<TChangePassForm>,
+  ) => {
+    const res = await changePassword({
+      old_password,
+      password,
+      confirm_password,
+    });
+
+    if (res) {
+      toast.success('Password has been successfully changed!');
+      resetForm();
+    }
   };
 
   useEffect(() => {
@@ -66,7 +102,7 @@ export const PartnerProfile = () => {
 
   if (isChosenEstablishmentLoading) {
     return (
-      <div className="flex h-full flex-col items-center justify-center">
+      <div className="flex h-full min-h-[500px] flex-col items-center justify-center">
         <Typography variant="h2">Loading...</Typography>
       </div>
     );
@@ -88,43 +124,153 @@ export const PartnerProfile = () => {
 
   return (
     <div
-      className="scroll flex h-full flex-col gap-4 overflow-y-scroll"
+      className="flex h-full flex-col gap-16 overflow-y-scroll"
       style={{ scrollbarWidth: 'none' }}
     >
-      <div className="flex flex-1 flex-col gap-2">
-        <Typography variant="paragraph">
-          <strong>Establishment name:</strong> {chosenEstablishment?.name}
-        </Typography>
-        <Typography variant="paragraph">
-          <strong>Description:</strong> {chosenEstablishment?.description}
-        </Typography>
-        <Typography variant="paragraph">
-          <strong> Happy hours:</strong> {chosenEstablishment?.happy_hour_start}{' '}
-          - {chosenEstablishment?.happy_hour_end}
-        </Typography>
-        <Typography variant="paragraph">
-          <strong>Email:</strong> {chosenEstablishment?.email}
-        </Typography>
-      </div>
-      <div className="flex justify-end gap-2">
-        <Link
-          href={ESTABLISHMENT_EDIT_PATH}
-          className="rounded-md bg-theme-grey-150 px-4 py-4 transition-colors duration-200 hover:bg-theme-grey-200 active:bg-theme-grey-300"
+      <div className="flex items-center gap-2">
+        <Button
+          onClick={() => onTabChoose('info')}
+          type="button"
+          variant={chosenTab === 'info' ? 'primary' : 'outline'}
         >
-          Edit establishment
-        </Link>
-        <Button variant="delete" onClick={onModalOpen}>
-          Delete establishment
+          <RiInformationLine />
+          <Typography variant="paragraph">Info</Typography>
         </Button>
-        <Modal
-          query={establishmentId}
-          isSubmitting={isMutating}
-          onModalSubmit={onEstablishmentDelete}
-          close={onModalClose}
+        <Button
+          onClick={() => onTabChoose('security')}
+          type="button"
+          variant={chosenTab === 'security' ? 'primary' : 'outline'}
         >
-          Delete establishment
-        </Modal>
+          <RiLockPasswordLine />
+          <Typography variant="paragraph">Security</Typography>
+        </Button>
       </div>
+      {chosenTab === 'info' && (
+        <>
+          <div className="flex flex-1 flex-col gap-2">
+            <Typography variant="paragraph">
+              <strong>Establishment name:</strong> {chosenEstablishment?.name}
+            </Typography>
+            <Typography variant="paragraph">
+              <strong>Description:</strong> {chosenEstablishment?.description}
+            </Typography>
+            <Typography variant="paragraph">
+              <strong> Happy hours:</strong>{' '}
+              {chosenEstablishment?.happy_hour_start} -{' '}
+              {chosenEstablishment?.happy_hour_end}
+            </Typography>
+            <Typography variant="paragraph">
+              <strong>Email:</strong> {chosenEstablishment?.email}
+            </Typography>
+          </div>
+          <div className="flex justify-end gap-2">
+            <Link
+              href={ESTABLISHMENT_EDIT_PATH}
+              className="rounded-md bg-theme-grey-150 px-4 py-4 transition-colors duration-200 hover:bg-theme-grey-200 active:bg-theme-grey-300"
+            >
+              Edit establishment
+            </Link>
+          </div>
+        </>
+      )}
+      {chosenTab === 'security' && (
+        <>
+          <div className="flex flex-1 gap-10">
+            <Formik
+              initialValues={{
+                old_password: '',
+                password: '',
+                confirm_password: '',
+              }}
+              onSubmit={onFormSubmit}
+            >
+              {() => {
+                return (
+                  <Form className="flex w-1/2 flex-col gap-2">
+                    <div className="relative w-full">
+                      <Field
+                        title="Old password"
+                        name="old_password"
+                        type={showPassword === 'old' ? 'text' : 'password'}
+                        as={Input}
+                      />
+                      <ShowPassword
+                        state={showPassword === 'old'}
+                        onClick={() =>
+                          setShowPassword(prev =>
+                            prev === 'old' ? null : 'old',
+                          )
+                        }
+                      />
+                    </div>
+                    <div className="relative w-full">
+                      <Field
+                        title="New password"
+                        name="password"
+                        type={showPassword === 'new' ? 'text' : 'password'}
+                        as={Input}
+                      />
+                      <ShowPassword
+                        state={showPassword === 'new'}
+                        onClick={() =>
+                          setShowPassword(prev =>
+                            prev === 'new' ? null : 'new',
+                          )
+                        }
+                      />
+                    </div>
+                    <div className="relative w-full">
+                      <Field
+                        title="Confirm new password"
+                        name="confirm_password"
+                        type={showPassword === 'confirm' ? 'text' : 'password'}
+                        as={Input}
+                      />
+                      <ShowPassword
+                        state={showPassword === 'confirm'}
+                        onClick={() =>
+                          setShowPassword(prev =>
+                            prev === 'confirm' ? null : 'confirm',
+                          )
+                        }
+                      />
+                    </div>
+                    <SubmitButton isMutating={isPasswordChanging}>
+                      Change password
+                    </SubmitButton>
+                  </Form>
+                );
+              }}
+            </Formik>
+            {changePasswordError && (
+              <div className="flex flex-col gap-1">
+                {changePasswordError?.map(([key, value]) => (
+                  <Error key={key}>
+                    <Typography variant="caption" weight="semibold">
+                      {key}
+                    </Typography>
+                    {value}
+                  </Error>
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="flex items-center justify-between gap-2 rounded-md border-2 border-red-300 p-5">
+            <Typography variant="h5">Danger zone</Typography>
+            <Button variant="delete" onClick={onModalOpen}>
+              Delete establishment
+            </Button>
+            <Modal
+              query={establishmentId}
+              isSubmitting={isMutating}
+              onModalSubmit={onEstablishmentDelete}
+              close={onModalClose}
+            >
+              Delete establishment
+            </Modal>
+          </div>
+        </>
+      )}
     </div>
   );
 };
