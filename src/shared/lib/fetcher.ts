282cb7f751e1c38f17d8c6aa340be1a525d34335
Aktan <aktanmoldokeev123@gmail.com>
2024-06-24 14:17:58 +0600

    Revert "Bugfix/menu error processing"

--- src/shared/lib/fetcher.ts
+++ src/shared/lib/fetcher.ts
@@ -1,11 +1,7 @@
 import { drinkjoyApi } from '../services';
 
 export const fetcher = async (url: string) => {
-  try {
-    const { data } = await drinkjoyApi(url);
+  const { data } = await drinkjoyApi(url);
 
-    return data;
-  } catch (error: any) {
-    throw error.response.data.detail;
-  }
+  return data;
 };
